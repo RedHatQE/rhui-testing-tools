1937203646f71c91f3c57456ef4e055f9619af74
Vitaly Kuznetsov <vitty@redhat.com>
2012-10-23 13:50:30 +0200

    rhuimanager_cds.py: add cds to an existing cluster

--- rhuilib/rhuimanager_cds.py
+++ rhuilib/rhuimanager_cds.py
@@ -1,11 +1,12 @@
+import re
+
 from rhuilib.expect import *
 from rhuilib.rhuimanager import *
 
 
 class RHUIManagerCds:
     @staticmethod
-    def add_cds(connection, clustername, cdsname, hostname="", displayname=""):
-        RHUIManager.screen(connection, "cds")
+    def _add_cds_part1(connection, clustername, cdsname, hostname="", displayname=""):
         Expect.enter(connection, "a")
         Expect.expect(connection, "Hostname of the CDS to register:")
         Expect.enter(connection, cdsname)
@@ -13,10 +14,27 @@ class RHUIManagerCds:
         Expect.enter(connection, hostname)
         Expect.expect(connection, "Display name for the CDS.*:")
         Expect.enter(connection, displayname)
-        Expect.expect(connection, "Enter a CDS cluster name:")
-        Expect.enter(connection, clustername)
+
+    @staticmethod
+    def add_cds(connection, clustername, cdsname, hostname="", displayname=""):
+        RHUIManager.screen(connection, "cds")
+        RHUIManagerCds._add_cds_part1(connection, clustername, cdsname, hostname, displayname)
+        state = Expect.expect_list(connection, [(re.compile(".*Enter a CDS cluster name:.*", re.DOTALL), 1), (re.compile(".*Select a CDS cluster or enter a new one:.*", re.DOTALL), 2)])
+        if state == 1:
+            Expect.enter(connection, clustername)
+        else:
+            Expect.enter(connection, 'b')
+            RHUIManagerCds._add_cds_part1(connection, clustername, cdsname, hostname, displayname)
+            try:
+                select = Expect.match(connection, re.compile(".*\s+([0-9]+)\s*-\s*" + clustername + "\s*\r\n.*to abort:.*", re.DOTALL))
+                Expect.enter(connection, select[0])
+            except ExpectFailed:
+                Expect.enter(connection, "1")
+                Expect.expect(connection, "Enter a CDS cluster name:")
+                Expect.enter(connection, clustername)
         RHUIManager.proceed(connection)
-        RHUIManager.quit(connection)
+        Expect.expect(connection, "Successfully registered.*rhui \(cds\) =>.*")
+        Expect.enter(connection, "q")
 
     @staticmethod
     def delete_cds(connection, clustername, cdslist):
