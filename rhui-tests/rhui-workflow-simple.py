c66524e469140bfe96ca52180279c4ebe2d0160b
Vitaly Kuznetsov <vitty@redhat.com>
2012-10-24 15:38:12 +0200

    rhui-workflow-simple.py: add RH repo to client cert

--- rhui-tests/rhui-workflow-simple.py
+++ rhui-tests/rhui-workflow-simple.py
@@ -46,21 +46,23 @@ RHUIManagerCds.associate_repo_cds(rs.RHUA, "Cluster1", ["repo1", "repo2"])
 RHUIManagerRepo.upload_content(rs.RHUA, ["repo1", "repo2"], "/etc/rhui/confrpm")
 RHUIManagerSync.sync_cds(rs.RHUA, cdslist)
 
+RHUIManagerClient.generate_ent_cert(rs.RHUA, "Cluster1", ["repo1"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
+
 ##it's impossible to do cluster sync and individual cds sync at the same moment
 ##RHUIManagerSync.sync_cluster(rs.RHUA,["Cluster1"])
-
-RHUIManagerClient.generate_ent_cert(rs.RHUA, "Cluster1", ["repo1"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
-RHUIManagerClient.create_conf_rpm(rs.RHUA, "Cluster1", cdslist[0], "/root", "/root/cert-repo1.crt", "/root/cert-repo1.key", "repo1", "3.0")
-rs.RHUA.sftp.get("/root/repo1-3.0/build/RPMS/noarch/repo1-3.0-1.noarch.rpm", "/root/repo1-3.0-1.noarch.rpm")
-Util.install_rpm_from_master(rs.CLI[0], "/root/repo1-3.0-1.noarch.rpm")
-
 
 if args.cert:
     RHUIManagerEntitlements.upload_content_cert(rs.RHUA, args.cert)
     RHUIManagerRepo.add_rh_repo_by_product(rs.RHUA, ["Red Hat Enterprise Linux 6 Server - Supplementary from RHUI \(RPMs\)", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\)"])
     RHUIManagerRepo.add_rh_repo_by_repo(rs.RHUA, ["Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-i386\)", "Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-x86_64\)"])
     RHUIManagerRepo.add_rh_repo_all(rs.RHUA)
-    RHUIManagerSync.sync_repo(rs.RHUA, ["Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-i386\)", "Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-x86_64\)"])
+    RHUIManagerSync.sync_repo(rs.RHUA, ["Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-i386\)", "Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-x86_64\)", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+    RHUIManagerCds.associate_repo_cds(rs.RHUA, "Cluster1", ["Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+    RHUIManagerClient.generate_ent_cert(rs.RHUA, "Cluster1", ["repo1", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\)"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
+
+RHUIManagerClient.create_conf_rpm(rs.RHUA, "Cluster1", cdslist[0], "/root", "/root/cert-repo1.crt", "/root/cert-repo1.key", "repo1", "3.0")
+rs.RHUA.sftp.get("/root/repo1-3.0/build/RPMS/noarch/repo1-3.0-1.noarch.rpm", "/root/repo1-3.0-1.noarch.rpm")
+Util.install_rpm_from_master(rs.CLI[0], "/root/repo1-3.0-1.noarch.rpm")
 
 RHUIManagerIdentity.generate_new(rs.RHUA)
 RHUIManagerUsers.change_password(rs.RHUA, "admin", "admin2")
