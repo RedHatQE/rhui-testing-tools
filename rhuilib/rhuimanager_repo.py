0f7f9f314e568a196ca96592c303b428025f9f5c
Vitaly Kuznetsov <vitty@redhat.com>
2012-10-24 19:09:39 +0200

    rhuimanager_repo.py: proceed_with_check for add_custom_repo

--- rhuilib/rhuimanager_repo.py
+++ rhuilib/rhuimanager_repo.py
@@ -15,10 +15,19 @@ class RHUIManagerRepo:
         Expect.enter(connection, "c")
         Expect.expect(connection, "Unique ID for the custom repository.*:")
         Expect.enter(connection, reponame)
+        checklist = ["ID: " + reponame]
         Expect.expect(connection, "Display name for the custom repository.*:")
         Expect.enter(connection, displayname)
+        if displayname != "":
+            checklist.append("Name: " + displayname)
+        else:
+            checklist.append("Name: " + reponame)
         Expect.expect(connection, "Path at which the repository will be served.*:")
         Expect.enter(connection, path)
+        if path != "":
+            checklist.append("Path: " + path)
+        else:
+            checklist.append("Path: " + reponame)
         Expect.expect(connection, "Enter value.*:")
         Expect.enter(connection, checksum_alg)
         Expect.expect(connection, "Should the repository require an entitlement certificate to access\? \(y/n\)")
@@ -26,21 +35,37 @@ class RHUIManagerRepo:
         if entitlement == "y":
             Expect.expect(connection, "Path that should be used when granting an entitlement for this repository.*:")
             Expect.enter(connection, entitlement_path)
+            if entitlement_path != "":
+                checklist.append("Entitlement: " + entitlement_path)
+            else:
+                checklist.append("Entitlement: " + reponame)
         Expect.expect(connection, "packages are signed by a GPG key\? \(y/n\)")
-        if redhat_gpg or custom_gpg:
+        if redhat_gpg == "y" or custom_gpg:
             Expect.enter(connection, "y")
+            checklist.append("GPG Check Yes")
             Expect.expect(connection, "Will the repository be used to host any Red Hat GPG signed content\? \(y/n\)")
             Expect.enter(connection, redhat_gpg)
+            if redhat_gpg == "y":
+                checklist.append("Red Hat GPG Key: Yes")
+            else:
+                checklist.append("Red Hat GPG Key: No")
             Expect.expect(connection, "Will the repository be used to host any custom GPG signed content\? \(y/n\)")
             if custom_gpg:
                 Expect.enter(connection, "y")
                 Expect.expect(connection, "Enter the absolute path to the public key of the GPG keypair:")
                 Expect.enter(connection, custom_gpg)
+                Expect.expect(connection, "Would you like to enter another public key\? \(y/n\)")
+                Expect.enter(connection, "n")
+                checklist.append("Custom GPG Keys: '" + custom_gpg + "'")
             else:
                 Expect.enter(connection, "n")
+                checklist.append("Custom GPG Keys: \(None\)")
         else:
             Expect.enter(connection, "n")
-        RHUIManager.proceed(connection)
+            checklist.append("GPG Check No")
+            checklist.append("Red Hat GPG Key: No")
+
+        RHUIManager.proceed_with_check(connection, "The following repository will be created:", checklist)
         Expect.expect(connection, "Successfully created repository.*rhui \(repo\) =>")
         Expect.enter(connection, "q")
 
