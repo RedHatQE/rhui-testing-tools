3b09c78ffb5124c9a0a68ee80021348c00dbe3a0
milan <mkovacik@redhat.com>
2012-10-29 15:08:48 +0100

    introduced  test-rhui-tcms178434

---
+++ rhui-tests/test-rhui-tcms178434.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python -tt
+
+import argparse, nose, re
+
+from rhuilib.expect import *
+from rhuilib.rhuisetup import *
+from rhuilib.rhuimanager import *
+from rhuilib.rhuimanager_cds import *
+
+class test_178434(object):
+    def __init__(self):
+        argparser = argparse.ArgumentParser(description=\
+                '178434: Unregister a CDS belonging to a Single-node Cluster')
+        args = argparser.parse_args()
+        self.rs = RHUIsetup()
+        self.rs.setup_from_rolesfile()
+    def test_01_initial_run(self):
+        """[setup] check initial rhui-manager login"""
+        RHUIManager.initial_run(self.rs.RHUA)
+    def test_02_add_cds(self):
+        """[setup] check creating a single-node cds cluster"""
+        RHUIManagerCds.add_cds(self.rs.RHUA, "cluster-1",
+                self.rs.CDS[0].hostname)
+    def test_03_remove_cds(self):
+        """[setup] check removing a cds from single node cluster"""
+        RHUIManagerCds.delete_cds(self.rs.RHUA, "cluster-1", [self.rs.CDS[0].hostname])
+    def test_04_check_cluster(self):
+        """[test] check the cluster no longer exists"""
+        pattern = re.compile('No CDS instances are registered.')
+        output = RHUIManagerCds.list(self.rs.RHUA)
+        match = pattern.match(output)
+        nose.tools.ok_(match is not None)
+    def test_05_assert_pulp(self):
+        """[test] assert no CDSes are dumped by pulp-admin cds list"""
+        Expect.enter(self.rs.RHUA, 'pulp-admin -u admin -p admin cds list')
+        pattern = re.compile(".*pulp-admin -u admin -p admin cds list\r\n\+-+\+\r\n\s*CDS Instances\s*\r\n\+-+\+(\r\n)+(.*)\[.*@.*\][\$#]", re.DOTALL)
+        result = Expect.match(self.rs.RHUA, pattern, grouplist=[2])[0]
+        pattern = re.compile("^(\r\n)*$", re.DOTALL)
+        match = pattern.match(result)
+        nose.tools.ok_(match is not None)
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v', '--with-outputsave'])
+
+
+
+
