c1a94b13e4f1b77de3788f2ef69a3f3c1efe4131
milan <mkovacik@redhat.com>
2012-10-29 20:21:30 +0100

    added cds move command

--- rhuilib/rhuimanager_cds.py
+++ rhuilib/rhuimanager_cds.py
@@ -106,3 +106,39 @@ class RHUIManagerCds:
         ret = Expect.match(connection, pattern, grouplist=[2])[0]
         RHUIManager.quit(connection)
         return ret
+
+    @staticmethod
+    def select_cluster(connection, clustername, ok=True):
+        '''
+        tries to select a cluster; if no such cluster and OK, creates new
+        '''
+        select_pattern = re.compile(".*\s+([0-9]+)\s*-\s*" + clustername + \
+                "\s*\r\n.*to abort:.*", re.DOTALL)
+        try:
+            select = Expect.match(connection, select_pattern, timeout=2)
+            Expect.enter(connection, select[0])
+        except ExpectFailed as err:
+            if not ok:
+                raise err
+            Expect.enter(connection, "1")
+            pattern = re.compile( ".*Enter a (new\s*)? CDS cluster name:\r\n", re.DOTALL)
+            Expect.expect_list(connection, [(pattern, 0)])
+            Expect.enter(connection, clustername)
+
+    @staticmethod
+    def move_cdses(connection, cdses, clustername):
+        '''
+        move the CDSes to clustername
+        '''
+        RHUIManager.screen(connection, "cds")
+        Expect.enter(connection, "m")
+        RHUIManager.select(connection, cdses)
+        RHUIManagerCds.select_cluster(connection, clustername)
+        RHUIManager.proceed_with_check(connection,
+                "The following Content Delivery Servers will be moved to the %s cluster:\r\n.*-+" % clustername,
+                cdses)
+        pattern = re.compile("(.*)", re.DOTALL)
+        result = Expect.match(connection, pattern)[0]
+        if re.match("Error", result):
+            raise ExpectFailed("moving %s to %s" % (cdses, clustername))
+        RHUIManager.quit(connection)
