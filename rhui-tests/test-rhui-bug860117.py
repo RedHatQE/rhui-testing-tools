0cc118fdf3f0e93324a8f98b0277e3ba2a5d1d0e
Vitaly Kuznetsov <vitty@redhat.com>
2012-10-30 12:32:45 +0100

    test-rhui-bug860117.py: add comments and cleanup

--- rhui-tests/test-rhui-bug860117.py
+++ rhui-tests/test-rhui-bug860117.py
@@ -22,15 +22,17 @@ class test_bug_860117:
         self.rs.setup_from_rolesfile()
         
     def test_01_initial_run(self):
+        ''' Do initial rhui-manager run'''
         RHUIManager.initial_run(self.rs.RHUA)
 
     def test_02_add_cds(self):
+        ''' Add cdses '''
         nose.tools.assert_equal(len(self.rs.CDS), 2)
         RHUIManagerCds.add_cds(self.rs.RHUA, "Cluster1", self.rs.CDS[0].hostname)
         RHUIManagerCds.add_cds(self.rs.RHUA, "Cluster2", self.rs.CDS[1].hostname)
-        
 
     def test_03_add_custom_repos(self):
+        ''' Create custom repos '''
         RHUIManagerRepo.add_custom_repo(self.rs.RHUA, "repo1")
         RHUIManagerRepo.add_custom_repo(self.rs.RHUA, "repo2")
         RHUIManagerRepo.add_custom_repo(self.rs.RHUA, "repo3")
@@ -39,13 +41,26 @@ class test_bug_860117:
         RHUIManagerRepo.add_custom_repo(self.rs.RHUA, "repo6")
 
     def test_04_associate_repo_cds(self):
+        ''' Associate custom repos with Cluster1 '''
         RHUIManagerCds.associate_repo_cds(self.rs.RHUA, "Cluster1", ["repo1", "repo3", "repo6"])
 
     def test_05_generate_ent_cert(self):
+        ''' Generate entitlement certificate '''
         RHUIManagerClient.generate_ent_cert(self.rs.RHUA, "Cluster1", ["repo1", "repo3", "repo6"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
 
     def test_06_create_conf_rpm(self):
+        ''' Create configuration rpm '''
         RHUIManagerClient.create_conf_rpm(self.rs.RHUA, "Cluster1", self.rs.CDS[0].hostname, "/root", "/root/cert-repo1.crt", "/root/cert-repo1.key", "repo1", "3.0")
+
+    def test_07_remove_cds(self):
+        ''' Remove cdses '''
+        RHUIManagerCds.delete_cds(self.rs.RHUA, "Cluster1", [self.rs.CDS[0].hostname])
+        RHUIManagerCds.delete_cds(self.rs.RHUA, "Cluster2", [self.rs.CDS[1].hostname])
+
+    def test_08_delete_custom_repo(self):
+        ''' Delete custom repos '''
+        RHUIManagerRepo.delete_repo(self.rs.RHUA, ["repo1", "repo2", "repo3", "repo4", "repo5", "repo6"])
+
 
 if __name__ == "__main__":
     nose.run(defaultTest=__name__, argv=[__file__, '-v'])
