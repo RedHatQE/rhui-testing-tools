c3a47ac2cc7a07b80606cc961b1452c53a23df50
Vitaly Kuznetsov <vitty@redhat.com>
2012-11-06 16:33:01 +0100

    remote-add-rh-rpm.sh introduced (upload RH-signed rpm and add it to /etc/rhui-testing.yaml)

---
+++ scripts/remote-add-rh-rpm.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -e
+
+SSH_OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+HOST=$1
+KEY=$2
+RPM=$3
+
+usage() {
+    echo "Usage: $0 <master hostname> <keyfile>  <RH rpm for testing>"
+    exit 1
+}
+
+if [ -z "$RPM" ] || [ ! -f "$RPM" ]; then
+    usage
+fi
+
+if [ ! -f "$KEY" ]; then
+    usage
+fi
+
+scp $SSH_OPT -r -i "$KEY" "$RPM" "root@$HOST:/var/lib/rhui-testing-tools/"
+ssh $SSH_OPT -i "$KEY" "root@$HOST" "rhui-add-testing-parameter.py --param rhrpm --value /var/lib/rhui-testing-tools/`basename $RPM`"
