67c0dea2412440e9c7fa3022e159e2714f5d05eb
Vitaly Kuznetsov <vitty@redhat.com>
2012-11-08 16:20:56 +0100

    test-rhui-workflow-simple.py: use smaller repos

--- rhui-tests/test-rhui-workflow-simple.py
+++ rhui-tests/test-rhui-workflow-simple.py
@@ -52,31 +52,32 @@ class test_workflow_simple(RHUITestcase):
         '''[Simple Workflow test] Generate entitlement certificate '''
         RHUIManagerClient.generate_ent_cert(self.rs.RHUA, "Cluster1", ["repo1"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
 
-        '''[Simple Workflow test] Sync cluster '''
+        #'''[Simple Workflow test] Sync cluster '''
         ##it's impossible to do cluster sync and individual cds sync at the same moment
         ##RHUIManagerSync.sync_cluster(self.rs.RHUA,["Cluster1"])
-        pass
 
         '''[Simple Workflow test] Upload RH content certificate '''
         RHUIManagerEntitlements.upload_content_cert(self.rs.RHUA, self.cert)
 
-        '''[Simple Workflow test] Add rh repo by product '''
-        RHUIManagerRepo.add_rh_repo_by_product(self.rs.RHUA, ["Red Hat Enterprise Linux 6 Server - Supplementary from RHUI \(RPMs\)", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\)"])
+        #'''[Simple Workflow test] Add rh repo by product '''
+        ## skipping as this repos are really big
+        ## RHUIManagerRepo.add_rh_repo_by_product(self.rs.RHUA, ["Red Hat Enterprise Linux 6 Server - Supplementary from RHUI \(RPMs\)", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\)"])
 
         '''[Simple Workflow test] Add rh repo by repo '''
-        RHUIManagerRepo.add_rh_repo_by_repo(self.rs.RHUA, ["Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-i386\)", "Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-x86_64\)"])
+        RHUIManagerRepo.add_rh_repo_by_repo(self.rs.RHUA, ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
 
-        '''[Simple Workflow test] Add all rh products '''
-        RHUIManagerRepo.add_rh_repo_all(self.rs.RHUA)
+        ## '''[Simple Workflow test] Add all rh products '''
+        ## RHUIManagerRepo.add_rh_repo_all(self.rs.RHUA)
 
         '''[Simple Workflow test] Syncronize repo '''
-        RHUIManagerSync.sync_repo(self.rs.RHUA, ["Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-i386\)", "Red Hat Enterprise Linux 5 Server from RHUI \(RPMs\) \(5Server-x86_64\)", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+        self._sync_repo(["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
+        self._sync_cds([self.rs.CDS[0].hostname])
 
         '''[Simple Workflow test] Associate rh repo with cds '''
-        RHUIManagerCds.associate_repo_cds(self.rs.RHUA, "Cluster1", ["Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+        RHUIManagerCds.associate_repo_cds(self.rs.RHUA, "Cluster1", ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
 
         '''[Simple Workflow test] Generate entitlement certificate with rh content '''
-        RHUIManagerClient.generate_ent_cert(self.rs.RHUA, "Cluster1", ["repo1", "Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\)"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
+        RHUIManagerClient.generate_ent_cert(self.rs.RHUA, "Cluster1", ["repo1", "Red Hat Update Infrastructure 2 \(RPMs\)"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
 
         '''[Simple Workflow test] Create configuration rpm '''
         RHUIManagerClient.create_conf_rpm(self.rs.RHUA, "Cluster1", self.rs.CDS[0].hostname, "/root", "/root/cert-repo1.crt", "/root/cert-repo1.key", "repo1", "3.0")
@@ -98,11 +99,10 @@ class test_workflow_simple(RHUITestcase):
             RHUIManagerCds.delete_cds(self.rs.RHUA, "Cluster1", [cds.hostname])
 
         '''[Simple Workflow cleanup] Delete custom repos '''
-        RHUIManagerRepo.delete_repo(self.rs.RHUA, ["repo1", "repo2"])
+        RHUIManagerRepo.delete_repo(self.rs.RHUA, ["repo1", "repo2", "Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
 
         '''[Simple Workflow cleanup] Remove rh certificate '''
         RHUIManager.remove_rh_certs(self.rs.RHUA)
-
 
 
 if __name__ == "__main__":
