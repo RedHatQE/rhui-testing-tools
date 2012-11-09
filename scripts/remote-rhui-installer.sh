07cd23af969753440b12f9cf3cd63e50ad2944a6
milan <mkovacik@redhat.com>
2012-11-09 14:59:14 +0100

    refactoring + calling easy_install pinocchio

--- scripts/remote-rhui-installer.sh
+++ scripts/remote-rhui-installer.sh
@@ -1,11 +1,13 @@
 #! /bin/bash -e
 
-SSH_OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-
 HOST=$1
 KEY=$2
 ISO=$3
 RPM=$4
+
+# shared ssh options
+SSH_OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile=$KEY"
+
 
 usage() {
     echo "Usage: $0 <master hostname> <keyfile> <RHUI ISO> <rhui-testing-tools.rpm>"
@@ -24,5 +26,23 @@ if [ ! -f "$KEY" ]; then
     usage
 fi
 
-scp $SSH_OPT -r -i "$KEY" "$ISO" "$RPM" "root@$HOST:/root/"
-ssh $SSH_OPT -i "$KEY" "root@$HOST" "yum install -y /root/`basename $RPM`; rhui-installer.py --iso /root/`basename $ISO`"
+function rexec() {
+    # root remotelly execute stuff via ssh
+    ssh $SSH_OPT "root@$HOST" "${@:?no command specified}"
+}
+
+function rcopy() {
+    # root copy remotelly stuff via scp
+    # $1:       destination folder
+    # $@ - $1:  sources
+    local destination=$1
+    shift
+    scp $SSH_OPT -r ${@:?no source specified} root@$HOST:${destination}
+}
+
+# copy the stuff
+rcopy /root "$ISO" "$RPM"
+
+rexec yum install -y /root/`basename $RPM`
+rexec rhui-installer.py --iso /root/`basename $ISO`
+rexec easy_install pinocchio
