85cdb9a20186a4a1dcf8496a71fa35a89aa77aeb
Vitaly Kuznetsov <vitty@redhat.com>
2012-11-13 12:17:09 +0100

    test-rhui-tcms178475.py introduced

---
+++ rhui-tests/test-rhui-tcms178475.py
@@ -0,0 +1,72 @@
+#! /usr/bin/python -tt
+
+import nose
+
+from rhuilib.util import *
+from rhuilib.rhui_testcase import *
+from rhuilib.rhuimanager import *
+from rhuilib.rhuimanager_cds import *
+from rhuilib.rhuimanager_client import *
+from rhuilib.rhuimanager_repo import *
+from rhuilib.rhuimanager_sync import *
+from rhuilib.rhuimanager_entitlements import *
+from rhuilib.cds import RhuiCds
+
+
+class test_tcms_178475(RHUITestcase, RHUI_has_RH_cert):
+    def _setup(self):
+
+        '''[TCMS#178475 setup] Do initial rhui-manager run'''
+        RHUIManager.initial_run(self.rs.RHUA)
+
+        '''[TCMS#178475 setup] Add cdses '''
+        RHUIManagerCds.add_cds(self.rs.RHUA, "Cluster1", self.rs.CDS[0].private_hostname, self.rs.CDS[0].public_hostname)
+
+        '''[TCMS#178475 setup] Upload RH content certificate '''
+        RHUIManagerEntitlements.upload_content_cert(self.rs.RHUA, self.cert)
+
+        '''[TCMS#178475 setup] Add rh repo '''
+        RHUIManagerRepo.add_rh_repo_by_repo(self.rs.RHUA, ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
+
+        '''[TCMS#178475 setup] Associate repo with cluster '''
+        RHUIManagerCds.associate_repo_cds(self.rs.RHUA, "Cluster1", ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
+
+        '''[TCMS#178475 setup] Sync RH repo '''
+        self._sync_repo(["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
+
+        '''[TCMS#178475 setup] Sync cds '''
+        self._sync_cds([self.rs.CDS[0].private_hostname])
+
+        '''[TCMS#178475 setup] Remove rhui configuration rpm from RHUA '''
+        Util.remove_conf_rpm(self.rs.CLI[0])
+
+    def _test(self):
+        '''[TCMS#178475 test] Generate entitlement certificate '''
+        RHUIManagerClient.generate_ent_cert(self.rs.RHUA, "Cluster1", ["Red Hat Update Infrastructure 2 \(RPMs\)"], "cert-repo1", "/root/", validity_days="", cert_pw=None)
+
+        '''[TCMS#178475 test] Create configuration rpm '''
+        RHUIManagerClient.create_conf_rpm(self.rs.RHUA, "Cluster1", self.rs.CDS[0].public_hostname, "/root", "/root/cert-repo1.crt", "/root/cert-repo1.key", "repo1", "3.0")
+
+        '''[TCMS#178475 test] Install configuration rpm to client '''
+        self.rs.RHUA.sftp.get("/root/repo1-3.0/build/RPMS/noarch/repo1-3.0-1.noarch.rpm", "/root/repo1-3.0-1.noarch.rpm")
+        Util.install_rpm_from_master(self.rs.CLI[0], "/root/repo1-3.0-1.noarch.rpm")
+
+        '''[TCMS#178475 test] Installing RH rpm from RH repo to the client '''
+        Expect.ping_pong(self.rs.CLI[0], "yum install -y pymongo && echo SUCCESS", "[^ ]SUCCESS", 60)
+
+    def _cleanup(self):
+        '''[TCMS#178475 cleanup] Removing RH rpm from RH repo from the client '''
+        Expect.ping_pong(self.rs.CLI[0], "rpm -e pymongo && echo SUCCESS", "[^ ]SUCCESS", 60)
+
+        '''[TCMS#178475 cleanup] Remove cdses '''
+        RHUIManagerCds.delete_cds(self.rs.RHUA, "Cluster1", [self.rs.CDS[0].private_hostname])
+
+        '''[TCMS#178475 cleanup] Delete custom repos '''
+        RHUIManagerRepo.delete_repo(self.rs.RHUA, ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
+        
+        '''[TCMS#178475 cleanup] Remove RH certificate from RHUI '''
+        RHUIManager.remove_rh_certs(self.rs.RHUA)
+
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
