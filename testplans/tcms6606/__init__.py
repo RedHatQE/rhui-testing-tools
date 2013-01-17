69b584c2b9500128beb78fc4c248c423aadda04f
Vitaly Kuznetsov <vitty@redhat.com>
2013-01-17 18:17:55 +0100

    test_rhui_tcms138202.py introduced

--- testplans/tcms6606/__init__.py
+++ testplans/tcms6606/__init__.py
@@ -15,4 +15,5 @@ from test_rhui_tcms90966 import *
 from test_rhui_tcms90968 import *
 from test_rhui_tcms90969 import *
 from test_rhui_tcms110761 import *
+from test_rhui_tcms138202 import *
 from test_rhui_tcms138854 import *
