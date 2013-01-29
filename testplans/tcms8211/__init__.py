cb1c935449714d8b2f2199366e7be641d9965bdf
Vitaly Kuznetsov <vitty@redhat.com>
2013-01-29 14:04:08 +0100

    test_rhui_bug860494.py introduced

--- testplans/tcms8211/__init__.py
+++ testplans/tcms8211/__init__.py
@@ -3,4 +3,5 @@ sys.path.append("/usr/share/rhui-testing-tools/rhui-tests")
 
 from test_rhui_bug811796 import *
 from test_rhui_bug856637 import *
+from test_rhui_bug860494 import *
 from test_rhui_bug867803 import *
