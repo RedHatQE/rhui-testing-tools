2cd316b619e4f3af506fa062df335110884d335e
Vitaly Kuznetsov <vitty@redhat.com>
2013-02-04 15:18:34 +0100

    test_rhui_bug862016.py introduced

---
+++ rhui-tests/test_rhui_bug862016.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python -tt
+
+import nose
+import re
+
+from patchwork.expect import *
+from rhuilib.util import *
+from rhuilib.rhui_testcase import *
+
+
+class test_bug_862016(RHUITestcase, RHUI_has_PROXY):
+    def _setup(self):
+        pass
+
+    def _test(self):
+        '''[Bug#862016 setup] No yum section in /etc/rhui/rhui-tools.conf '''
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep \"\[yum\]\" /etc/rhui/rhui-tools.conf", 1)
+
+        '''[Bug#862016 setup] redhat section in /etc/rhui/rhui-tools.conf '''
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep -B3 proxy_ /etc/rhui/rhui-tools.conf | grep \"\[redhat\]\"")
+
+    def _cleanup(self):
+        pass
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
--- testplans/tcms8211/__init__.py
+++ testplans/tcms8211/__init__.py
@@ -6,6 +6,7 @@ from test_rhui_bug821319 import *
 from test_rhui_bug848885 import *
 from test_rhui_bug856637 import *
 from test_rhui_bug860494 import *
+from test_rhui_bug862016 import *
 from test_rhui_bug865822 import *
 from test_rhui_bug867803 import *
 from test_rhui_bug871497 import *
