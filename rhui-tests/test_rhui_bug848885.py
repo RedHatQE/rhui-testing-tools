31400a44d9c5ee244c614c395c1b9a3f0dac9acd
Vitaly Kuznetsov <vitty@redhat.com>
2013-02-04 15:18:19 +0100

    test_rhui_bug848885.py: upgrade

--- rhui-tests/test_rhui_bug848885.py
+++ rhui-tests/test_rhui_bug848885.py
@@ -28,8 +28,16 @@ class test_bug_848885(RHUITestcase, RHUI_has_PROXY):
         Expect.ping_pong(self.rs.Instances["RHUA"][0], "rpm -U /etc/rhui/confrpm/" + self.rs.Instances["RHUA"][0].private_hostname + "-" + version_new + "-*.rpm && echo SUCCESS", "[^ ]SUCCESS", 60)
 
     def _test(self):
-        '''[Bug#848885 setup] Check for proxy_port/proxy_pass settings (shouldn't be set) '''
-        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep -P \"^(proxy_port|proxy_pass)\" /etc/pulp/pulp.conf  /etc/rhui/rhui-tools.conf", 1)
+        '''[Bug#848885 test] Check for proxy_port/proxy_pass settings (shouldn't be set) '''
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep -P \"^(proxy_port|proxy_pass)\" /etc/pulp/pulp.conf /etc/rhui/rhui-tools.conf", 1)
+
+        '''[Bug#848885 test] Check for proxy_host setting (should be set) '''
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep \"proxy_host\" /etc/pulp/pulp.conf", 1)
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep \"proxy_host\" /etc/rhui/rhui-tools.conf", 1)
+
+        '''[Bug#848885 test] Check for proxy_host setting (should be set) '''
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep \"proxy_user\" /etc/pulp/pulp.conf", 1)
+        Expect.expect_retval(self.rs.Instances["RHUA"][0], "grep \"proxy_user\" /etc/rhui/rhui-tools.conf", 1)
 
     def _cleanup(self):
         '''[Bug#848885 cleanup] Restore configuration RPM '''
