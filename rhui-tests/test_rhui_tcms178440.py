2d645878858165d717f9c141f8f7e2641439a355
pbartiko <p.bartikova@gmail.com>
2013-02-07 14:52:05 +0100

    added test_rhui_tcms178440.py

---
+++ rhui-tests/test_rhui_tcms178440.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python -tt
+
+import nose
+import re
+
+from patchwork.expect import *
+from rhuilib.rhui_testcase import *
+from rhuilib.rhuimanager import *
+from rhuilib.rhuimanager_cds import *
+from rhuilib.pulp_admin import *
+from rhuilib.cds import *
+
+
+class test_tcms_178440(RHUITestcase):
+    def _setup(self):
+        '''[TCMS#178440 setup] Do initial rhui-manager run'''
+        RHUIManager.initial_run(self.rs.Instances["RHUA"][0])
+
+        '''[TCMS#178440 setup] Add cdses'''
+        RHUIManagerCds.add_cds(self.rs.Instances["RHUA"][0], "Cluster1", self.rs.Instances["CDS"][0].private_hostname)
+        RHUIManagerCds.add_cds(self.rs.Instances["RHUA"][0], "Cluster1", self.rs.Instances["CDS"][1].private_hostname)
+        
+        '''[TCMS#178440 setup] Remove a cds'''
+        RHUIManagerCds.delete_cds(self.rs.Instances["RHUA"][0], "Cluster1", [self.rs.Instances["CDS"][0].private_hostname])
+
+    def _test(self):
+        '''[TCMS#178440 test] Check the cds info screen'''
+        rhui_cds = RhuiCds(
+                           hostname=self.rs.Instances["CDS"][1].private_hostname,
+                           description='RHUI CDS',
+                           cluster='Cluster1',
+                           repos=[])
+
+        nose.tools.assert_equal(RHUIManagerCds.info(self.rs.Instances["RHUA"][0], ["Cluster1"]), [rhui_cds])
+
+        '''[TCMS#178440 test] Check pulp-admin and rhui cluster info are the same'''
+        nose.tools.assert_equal(PulpAdmin.cds_list(self.rs.Instances["RHUA"][0]), RHUIManagerCds.info(self.rs.Instances["RHUA"][0], ['Cluster1']))
+
+    def _cleanup(self):
+        '''[TCMS#178440 cleanup] Remove a cds'''
+        RHUIManagerCds.delete_cds(self.rs.Instances["RHUA"][0], "Cluster1", [self.rs.Instances["CDS"][1].private_hostname])
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
--- testplans/tcms6610/__init__.py
+++ testplans/tcms6610/__init__.py
@@ -7,6 +7,7 @@ from test_rhui_tcms178428 import *
 from test_rhui_tcms178434 import *
 from test_rhui_tcms178438 import *
 from test_rhui_tcms178439 import *
+from test_rhui_tcms178440 import *
 from test_rhui_tcms178442 import *
 from test_rhui_tcms178462 import *
 from test_rhui_tcms178463 import *
