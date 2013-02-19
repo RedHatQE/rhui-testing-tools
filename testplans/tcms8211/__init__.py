2f451f17e5bf0f5613c1331efdf327f110995048
Vitaly Kuznetsov <vitty@redhat.com>
2013-02-19 14:54:40 +0100

    remove meaningless comments

--- testplans/tcms8211/__init__.py
+++ testplans/tcms8211/__init__.py
@@ -1,18 +1,6 @@
 import sys
 sys.path.append("/usr/share/rhui-testing-tools/rhui-tests")
 
-#renamed:    test_rhui_bug811796.py -> .py
-#renamed:    test_rhui_bug848885.py -> .py
-#renamed:    test_rhui_bug856637.py -> .py
-#renamed:    test_rhui_bug860494.py -> .py
-#renamed:    test_rhui_bug865822.py -> .py
-#renamed:    test_rhui_bug867803.py -> .py
-#renamed:    test_rhui_bug871497.py -> .py
-#renamed:    test_rhui_bug879546.py -> .py
-#renamed:    test_rhui_bug821319.py -> .py
-#renamed:    test_rhui_bug892488.py -> .py
-#renamed:    test_rhui_bug815975.py -> .py
-#renamed:    test_rhui_bug845941.py -> .py
 from test_rhui_tcms236641 import *
 from test_rhui_tcms240397 import *
 from test_rhui_tcms236759 import *
