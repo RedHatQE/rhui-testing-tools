2f5dd2d5621758e2aa65536ba39b70ff4a56a7b3
Vitaly Kuznetsov <vitty@redhat.com>
2013-02-27 10:10:54 +0100

    test_rhui_nonclean_setup_rhel6.py introduced (0day testing)

---
+++ rhui-tests/test_rhui_nonclean_setup_rhel6.py
@@ -0,0 +1,62 @@
+#! /usr/bin/python -tt
+
+import nose
+
+from rhuilib.util import *
+from rhuilib.rhui_testcase import *
+from rhuilib.rhuimanager import *
+from rhuilib.rhuimanager_cds import *
+from rhuilib.rhuimanager_client import *
+from rhuilib.rhuimanager_repo import *
+from rhuilib.rhuimanager_sync import *
+from rhuilib.rhuimanager_entitlements import *
+from rhuilib.cds import RhuiCds
+
+
+class test_nonclean_rhel6(RHUITestcase, RHUI_has_RH_cert, RHUI_has_RHEL6_CLI):
+    def _setup(self):
+
+        '''[RHEL6 nonclean setup] Do initial rhui-manager run'''
+        RHUIManager.initial_run(self.rs.Instances["RHUA"][0])
+
+        '''[RHEL6 nonclean setup] Add cds '''
+        RHUIManagerCds.add_cds(self.rs.Instances["RHUA"][0], "Cluster1", self.rs.Instances["CDS"][0].private_hostname)
+
+        '''[RHEL6 nonclean setup] Upload RH content certificate '''
+        RHUIManagerEntitlements.upload_content_cert(self.rs.Instances["RHUA"][0], self.cert)
+
+        '''[RHEL6 nonclean setup] Remove rhui configuration rpm from clients '''
+        Util.remove_conf_rpm(self.rhel6client)
+
+        '''[RHEL6 nonclean setup] Add rh repo '''
+        RHUIManagerRepo.add_rh_repo_by_repo(self.rs.Instances["RHUA"][0], ["Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+
+        '''[RHEL6 nonclean setup] Associate repo with clusters '''
+        RHUIManagerCds.associate_repo_cds(self.rs.Instances["RHUA"][0], "Cluster1", ["Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+
+        '''[RHEL6 nonclean setup] Sync RH repo '''
+        self._sync_repo(["Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\) \(6Server-x86_64\)"])
+
+        '''[RHEL6 nonclean setup] Sync cds '''
+        self._sync_cds([self.rs.Instances["CDS"][0].private_hostname])
+
+        '''[RHEL6 nonclean setup] Generate entitlement certificates '''
+        RHUIManagerClient.generate_ent_cert(self.rs.Instances["RHUA"][0], "Cluster1", ["Red Hat Enterprise Linux 6 Server from RHUI \(RPMs\)"], "cert1", "/root/", validity_days="", cert_pw=None)
+
+        '''[RHEL6 nonclean setup] Create configuration rpms '''
+        RHUIManagerClient.create_conf_rpm(self.rs.Instances["RHUA"][0], "Cluster1", self.rs.Instances["CDS"][0].private_hostname, "/root", "/root/cert1.crt", "/root/cert1.key", "repo1", "3.0")
+
+        '''[RHEL6 nonclean setup] Install configuration rpms to the clients '''
+        Util.install_rpm_from_rhua(self.rs.Instances["RHUA"][0], self.rhel6client, "/root/repo1-3.0/build/RPMS/noarch/repo1-3.0-1.noarch.rpm")
+
+    def _test(self):
+        '''[RHEL6 nonclean test] Installing RH rpm to the clients '''
+        Expect.ping_pong(self.rhel6client, "yum install -y zsh && echo SUCCESS", "[^ ]SUCCESS", 60)
+
+    def _cleanup(self):
+        '''[RHEL6 nonclean cleanup] Removing RH rpms from the clients '''
+        Expect.ping_pong(self.rhel6client, "rpm -e zsh && echo SUCCESS", "[^ ]SUCCESS", 60)
+ 
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
