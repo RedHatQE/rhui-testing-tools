6f5dbd0b7b336d1cb228bcf2ca2cb61ad7fe7d91
Ina Panova <ipanova@redhat.com>
2013-05-16 17:10:29 +0200

    adjustment of formatting

--- rhui-tests/test_rhui_tcms267060.py
+++ rhui-tests/test_rhui_tcms267060.py
@@ -11,7 +11,7 @@ from patchwork.expect import *
 class test_tcms267060(RHUITestcase):
     def _setup(self):
         pass
-    
+
     def _test(self):
         '''[TCMS#267060 test] generate new identity certificate'''
         RHUIManager.screen(self.rs.Instances["RHUA"][0], "identity")
@@ -22,24 +22,25 @@ class test_tcms267060(RHUITestcase):
         Expect.enter(self.rs.Instances["RHUA"][0], "")
         Expect.expect(self.rs.Instances["RHUA"][0], "Enter pass phrase for.*:")
         Expect.enter(self.rs.Instances["RHUA"][0], "")
-        
+
         '''[TCMS#267060 test] send to sleep the process pressing CRTL-Z'''
         self.rs.Instances["RHUA"][0].exec_command("kill -SIGTSTP `ps ax | grep rhui-manager | grep -v grep | cut -f 1 -d ' '`")
         self.rs.Instances["RHUA"][0].exec_command("fg %1")
         Expect.enter(self.rs.Instances["RHUA"][0], "")
-        '''[TCMS#267060 test] check whether or not the process entered infinite loop''' 
+
+        '''[TCMS#267060 test] check whether or not the process entered infinite loop'''
         Expect.expect(self.rs.Instances["RHUA"][0], ".*=>.*")
         Expect.enter(self.rs.Instances["RHUA"][0], "q")
-              
+
     def _cleanup(self):
-       
+
         '''[TCMS#267060 cleanup] kill rhui-manager '''
         self.rs.Instances["RHUA"][0].exec_command("kill -SIGKILL `ps ax | grep rhui-manager | grep -v grep | cut -f 1 -d ' '`")
-        
+
         '''[TCMS#267060 cleanup] check rhui-manager is killed via finding the prompt of RHUA instance'''
         Expect.enter(self.rs.Instances["RHUA"][0], "")
         Expect.expect(self.rs.Instances["RHUA"][0],"root@rhua.*")
-        
-        
+
+
 if __name__ == "__main__":
     nose.run(defaultTest=__name__, argv=[__file__, '-v'])
