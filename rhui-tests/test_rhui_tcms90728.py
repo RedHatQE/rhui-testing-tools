e938228b086a0660ae0a94ce429f73ba1fe6433f
pbartiko <p.bartikova@gmail.com>
2013-06-26 17:21:17 +0200

    modified test_rhui_tcms90728.py

--- rhui-tests/test_rhui_tcms90728.py
+++ rhui-tests/test_rhui_tcms90728.py
@@ -14,9 +14,13 @@ class test_rhui_tcms90728(RHUITestcase):
         '''[TCMS#90728 setup] Do initial rhui-manager run'''
         RHUIManager.initial_run(self.rs.Instances["RHUA"][0])
 
-        '''[TCMS#90728 setup] Create custom repo'''
+        '''[TCMS#90728 setup] Create repos'''
         RHUIManagerRepo.add_custom_repo(self.rs.Instances["RHUA"][0], "repo1")
         RHUIManagerRepo.add_custom_repo(self.rs.Instances["RHUA"][0], "repo2")
+        RHUIManagerRepo.add_rh_repo_by_repo(self.rs.Instances["RHUA"][0], ["Red Hat Enterprise Linux 6 Server - Optional Beta from RHUI\(Debug RPMs\) \(6Server-i386\)"])
+        
+        '''[TCMS#284279 setup] Sync RH repo '''
+        self._sync_repo(["Red Hat Enterprise Linux 6 Server - Optional Beta from RHUI\(Debug RPMs\) \(6Server-i386\)"])
         
         '''[TCMS#90728 setup] Upload content'''
         self.rs.Instances["RHUA"][0].sftp.put("/usr/share/rhui-testing-tools/testing-data/custom-signed-rpm-1-0.1.fc17.noarch.rpm", "/root/custom-signed-rpm-1-0.1.fc17.noarch.rpm")
@@ -29,11 +33,13 @@ class test_rhui_tcms90728(RHUITestcase):
         nose.tools.assert_equal(RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "repo1", "custom-unsigned"), ["custom-unsigned-rpm-1-0.1.fc17.noarch.rpm"])
         nose.tools.assert_equal(RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "repo1", "test"), [])
         nose.tools.assert_equal(RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "repo2", ""), [])
+        nose.tools.assert_equal(RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "Red Hat Enterprise Linux 6 Server - Optional Beta from RHUI\(Debug RPMs\) \(6Server-i386\)", ""), ["Package Count: 124"])
 
     def _cleanup(self):
         '''[TCMS#90728 cleanup] Remove a repo'''
         RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["repo1"])
         RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["repo2"])
+        RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["Red Hat Enterprise Linux 6 Server - Optional Beta from RHUI\(Debug RPMs\) \(6Server-i386\)"])
         
 if __name__ == "__main__":
-    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
+    nose.run(defaultTest=__name__, argv=[__file__, '--with-outputsave'])
