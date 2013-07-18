40508e1a0c4e4ffd94cf002b7898717b97f02a23
pbartiko <p.bartikova@gmail.com>
2013-07-18 17:17:36 +0200

    correction

--- rhui-tests/test_rhui_tcms293038.py
+++ rhui-tests/test_rhui_tcms293038.py
@@ -10,16 +10,16 @@ from rhuilib.rhuimanager_cds import *
 from rhuilib.pulp_admin import *
 from rhuilib.cds import *
 
-class test_tcms_290038(RHUITestcase, RHUI_has_two_CDSes):
+class test_tcms_293038(RHUITestcase, RHUI_has_two_CDSes):
     def _setup(self):
-        '''[TCMS#290038 setup] Do initial rhui-manager run'''
+        '''[TCMS#293038 setup] Do initial rhui-manager run'''
         RHUIManager.initial_run(self.rs.Instances["RHUA"][0])
 
-        '''[TCMS#290038 setup] Add cds'''
+        '''[TCMS#293038 setup] Add cds'''
         RHUIManagerCds.add_cds(self.rs.Instances["RHUA"][0], "Cluster1", self.rs.Instances["CDS"][0].private_hostname)
 
     def _test(self):
-        '''[TCMS#290038 test] Check CDS info screen'''
+        '''[TCMS#293038 test] Check CDS info screen'''
         rhui_cds = RhuiCds(
                            hostname=self.rs.Instances["CDS"][0].private_hostname,
                            description='RHUI CDS',
@@ -28,11 +28,11 @@ class test_tcms_290038(RHUITestcase, RHUI_has_two_CDSes):
 
         nose.tools.assert_equal(RHUIManagerCds.info(self.rs.Instances["RHUA"][0], ["Cluster1"]), [rhui_cds])
 
-        '''[TCMS#290038 test] Check pulp-admin and rhui cluster info are the same'''
+        '''[TCMS#293038 test] Check pulp-admin and rhui cluster info are the same'''
         nose.tools.assert_equal(PulpAdmin.cds_list(self.rs.Instances["RHUA"][0]), RHUIManagerCds.info(self.rs.Instances["RHUA"][0], ['Cluster1']))
 
     def _cleanup(self):
-        '''[TCMS#290038 cleanup] Remove cds'''
+        '''[TCMS#293038 cleanup] Remove cds'''
         RHUIManagerCds.delete_cds(self.rs.Instances["RHUA"][0], "Cluster1", [self.rs.Instances["CDS"][0].private_hostname])
 
 if __name__ == "__main__":
