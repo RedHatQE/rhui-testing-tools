0eb7c575fd99730069e57265b35e10cb49fdf8e1
Ina Panova <ipanova@redhat.com>
2013-11-25 13:54:15 +0100

    removed test_rhui_tcms267060 from tcms9352 testplan

--- testplans/tcms9352/__init__.py
+++ testplans/tcms9352/__init__.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.append("/usr/share/rhui-testing-tools/rhui-tests")
 
-from test_rhui_tcms267060 import *
 from test_rhui_tcms267309 import *
 from test_rhui_tcms267489 import *
 from test_rhui_tcms268363 import *
