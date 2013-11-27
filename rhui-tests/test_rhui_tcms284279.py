b6a427470ee4f7b2775f8c6f08315bcfcd0da2c7
Ina Panova <ipanova@redhat.com>
2013-11-27 11:42:16 +0100

    updated  test_rhui_tcms284279.py to test 3 scenarios

--- rhui-tests/test_rhui_tcms284279.py
+++ rhui-tests/test_rhui_tcms284279.py
@@ -9,6 +9,12 @@ from rhuilib.rhuimanager_entitlements import *
 
 
 class test_tcms284279(RHUITestcase, RHUI_has_RH_cert):
+    ''' for more ditails see bz 957128
+    3 scenarios has to be tested here:
+    a) upload fake rpms from a directory
+    b) upload fake rpm explicitly
+    c) upload signed rpm from directory'''
+
     def _setup(self):
         """[TCMS#284279 setup] Do initial rhui-manager run"""
         RHUIManager.initial_run(self.rs.Instances["RHUA"][0])
@@ -18,6 +24,8 @@ class test_tcms284279(RHUITestcase, RHUI_has_RH_cert):
 
         '''[TCMS#284279 setup] Create custom repo '''
         RHUIManagerRepo.add_custom_repo(self.rs.Instances["RHUA"][0], "repo284279")
+        RHUIManagerRepo.add_custom_repo(self.rs.Instances["RHUA"][0], "repo_xxx")
+        RHUIManagerRepo.add_custom_repo(self.rs.Instances["RHUA"][0], "repo_yyy")
 
         '''[TCMS#284279 setup] Add rh repo '''
         RHUIManagerRepo.add_rh_repo_by_repo(self.rs.Instances["RHUA"][0], ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
@@ -29,7 +37,7 @@ class test_tcms284279(RHUITestcase, RHUI_has_RH_cert):
         for i in xrange(10):
             Expect.enter(self.rs.Instances["RHUA"][0], "touch /var/lib/pulp/repos/content/dist/rhel/rhui/server/6/6Server/x86_64/rhui/2/os/Packages/test%d.rpm && echo SUCCESS" % i)
 
-        '''[TCMS#284279 setup] Upload content to custom repo '''
+        '''[TCMS#284279 setup] Upload content to custom repo284279 '''
         Expect.enter(self.rs.Instances["RHUA"][0], "q")
         RHUIManager.screen(self.rs.Instances["RHUA"][0], "repo")
         Expect.enter(self.rs.Instances["RHUA"][0], "u")
@@ -39,21 +47,52 @@ class test_tcms284279(RHUITestcase, RHUI_has_RH_cert):
         RHUIManager.proceed_without_check(self.rs.Instances["RHUA"][0])
         RHUIManager.quit(self.rs.Instances["RHUA"][0], "", 200)
 
+        """[TCMS#284279 setup] Create rpm files"""
+        self.rs.Instances["RHUA"][0].sftp.put("/usr/share/rhui-testing-tools/testing-data/custom-signed-rpm-1-0.1.fc17.noarch.rpm", "/root/custom-signed-rpm-1-0.1.fc17.noarch.rpm")
+        self.rs.Instances["RHUA"][0].sftp.put("/usr/share/rhui-testing-tools/testing-data/custom-unsigned-rpm-1-0.1.fc17.noarch.rpm", "/root/custom-unsigned-rpm-1-0.1.fc17.noarch.rpm")
+
+        '''[TCMS#284279 setup] Upload content to custom repo_xxx '''
+        RHUIManagerRepo.upload_content(self.rs.Instances["RHUA"][0], ["repo_xxx"], "/root")
+
+        """[TCMS#284279 setup] Create fake rpm"""
+        Expect.enter(self.rs.Instances["RHUA"][0], "touch /root/fake.rpm && echo SUCCESS")
+
+        '''[TCMS#284279 setup] Upload content to custom repo_yyy'''
+        RHUIManager.screen(self.rs.Instances["RHUA"][0], "repo")
+        Expect.enter(self.rs.Instances["RHUA"][0], "u")
+        RHUIManager.select(self.rs.Instances["RHUA"][0], ["repo_yyy"])
+        Expect.expect(self.rs.Instances["RHUA"][0], "will be uploaded:")
+        Expect.enter(self.rs.Instances["RHUA"][0], "/root/fake.rpm")
+        Expect.expect(self.rs.Instances["RHUA"][0], "error: not an rpm package")
+        Expect.enter(self.rs.Instances["RHUA"][0], "q")
+
     def _test(self):
-        '''[TCMS#284279 test] Check the packages list'''
+        '''[TCMS#284279 test] Check the packages list for repo284279'''
         expected_list = RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)", "")
         actual_list = RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "repo284279", "")
         nose.tools.assert_equal(len(set(expected_list) - set(actual_list)), 0)
 
+        '''[TCMS#284279 test] Check the packages list for repo_xxx'''
+        nose.tools.assert_equal(RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "repo_xxx", ""), ["custom-signed-rpm-1-0.1.fc17.noarch.rpm", "custom-unsigned-rpm-1-0.1.fc17.noarch.rpm"])
+
+        '''[TCMS#284279 test] Check the packages list for repo_yyy'''
+        nose.tools.assert_equal(RHUIManagerRepo.check_for_package(self.rs.Instances["RHUA"][0], "repo_yyy", ""), [])
+
     def _cleanup(self):
-        '''[TCMS#284279 cleanup] Delete custom repo '''
+        '''[TCMS#284279 cleanup] Delete custom repos '''
         RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["repo284279"])
+        RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["repo_xxx"])
+        RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["repo_yyy"])
 
         '''[TCMS#284279 cleanup] Delete RH repo '''
         RHUIManagerRepo.delete_repo(self.rs.Instances["RHUA"][0], ["Red Hat Update Infrastructure 2 \(RPMs\) \(6Server-x86_64\)"])
 
         '''[TCMS#284279 cleanup] Remove RH certs from RHUI '''
         RHUIManager.remove_rh_certs(self.rs.Instances["RHUA"][0])
+
+        '''[TCMS#284279 cleanup] Remove fake rpm from RHUI '''
+        Expect.ping_pong(self.rs.Instances["RHUA"][0], " rm -f /root/fake.rpm && echo SUCCESS", "[^ ]SUCCESS")
+
 
 if __name__ == "__main__":
     nose.run(defaultTest=__name__, argv=[__file__, '-v'])
